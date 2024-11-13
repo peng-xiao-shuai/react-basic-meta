cf12276efc4677cde406cac905f72532840523e3
peng-xiao-shuai <1612565136@qq.com>
2024-11-13 15:19:24 +0800

    pref: 抽离基类

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,42 +1,16 @@
-// react-basic-meta/src/utils/index.ts
-/**
- * @author: peng-xiao-shuai
- * @date: 2023-10-25 17:50:42
- * @last Modified by: peng-xiao-shuai
- * @last Modified time: 2023-10-25 17:50:42
- */
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons';
 import { unloadModel } from './other';
 import { PersonModel } from './person-model';
 import { GUI } from './gui';
 import { MOTION } from './constant';
+import { BaseScene } from './base-scene';
 
 interface InitThreeParams {
   id: string;
 }
 
-export class InitThree {
+export class InitThree extends BaseScene {
   params: InitThreeParams;
-  container: HTMLElement;
-  w_h: { w: number; h: number };
-  renderer: THREE.WebGLRenderer;
-  camera: THREE.PerspectiveCamera;
-  scene: THREE.Scene | null;
-  controls: OrbitControls | null;
-
-  /**
-   * requestAnimationFrame id, 用于停止
-   */
-  frameId: number;
-  /**
-   * 帧数之间时间
-   */
-  clock: THREE.Clock;
-  /**
-   * 分辨率
-   */
-  devicePixelRatio: number;
   /**
    * gui 实例
    */
@@ -45,61 +19,21 @@ export class InitThree {
    * 是否碰撞
    */
   // isCollision: boolean;
-  updateSize: () => void;
-
   personModelInstance: InstanceType<typeof PersonModel>;
 
   constructor(params: InitThreeParams) {
     if (!params.id) console.error('未传递id');
+    super(params.id);
     this.params = params;
-    this.container = document.getElementById(this.params.id) as HTMLElement;
-
-    this.w_h = {
-      w: this.container.clientWidth || window.innerWidth,
-      h: this.container.clientHeight || window.innerHeight,
-    };
-
-    this.renderer = new THREE.WebGLRenderer({
-      antialias: true,
-      alpha: true,
-    });
-    this.renderer.setClearColor('#000000');
-
-    this.camera = new THREE.PerspectiveCamera(55, this.w_h.w / this.w_h.h);
-
-    this.scene = new THREE.Scene();
-    this.controls = null;
-    this.frameId = 0;
-    this.clock = new THREE.Clock();
-    this.devicePixelRatio = window.devicePixelRatio || 1;
     // this.isCollision = false;
     this.personModelInstance = new PersonModel();
 
     // 创建一个 GUI
     this.gui = GUI.getInstance();
-
-    this.updateSize = () => {
-      const width = window.innerWidth;
-      const height = window.innerHeight;
-
-      // Update camera aspect ratio
-      this.camera.aspect = width / height;
-      this.camera.updateProjectionMatrix();
-
-      // Update renderer size
-      this.renderer.setSize(width, height);
-    };
   }
 
   init() {
-    const { w, h } = this.w_h;
-    this.renderer.setPixelRatio(this.devicePixelRatio);
-    this.renderer.setSize(w, h);
-    // 添加dom
-    this.container.appendChild(this.renderer.domElement);
-
-    // 关闭影子
-    this.renderer.shadowMap.enabled = false;
+    super.baseInit();
 
     // 环境光
     const ALight = new THREE.AmbientLight('#93cbe7', 5);
@@ -114,26 +48,10 @@ export class InitThree {
       const axesHelper = new THREE.AxesHelper(500);
       this.scene!.add(axesHelper);
     }
-
-    // 轨道控制器
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.maxPolarAngle = Math.PI / 2.2;
-    this.controls.minPolarAngle = Math.PI / 3;
-    this.controls.maxDistance = 30;
-    this.controls.minDistance = 25;
-
-    // this.controls.enableZoom = false
-    // this.controls.enablePan = false
-    // this.controls.enableDamping = false
-    // this.controls.dampingFactor = 0.05
     this.personModelInstance.render(this);
 
     this.guiInit();
-    this.resize();
     this.render();
-  }
-  resize() {
-    window.addEventListener('resize', this.updateSize);
   }
 
   guiInit() {
@@ -147,6 +65,7 @@ export class InitThree {
 
   unload() {
     unloadModel.call(this);
+    super.destroy();
     this.gui.destroy();
     this.container.removeChild(this.renderer.domElement);
   }
